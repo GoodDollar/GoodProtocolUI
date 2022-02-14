3adcc4bf2349e86820b3112e4d810c2bbd858bba
Devartjoker <devart@artjoker.net>
2022-02-14 12:03:08 +0300

    Change Switch to be the same in the app

--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -4,9 +4,10 @@ import { StyledSwitch } from './styled'
 type SwitchProps = Omit<React.HTMLAttributes<HTMLInputElement>, 'onChange'> & {
     onChange: (value: boolean) => void
     checked: boolean
+    disabled?: boolean
 }
 
-export default function Switch({ onChange, defaultChecked, checked, ...rest }: SwitchProps): JSX.Element {
+export default function Switch({ onChange, defaultChecked, checked, disabled, ...rest }: SwitchProps): JSX.Element {
     const onSwitchChange = useCallback(
         (e: React.ChangeEvent<HTMLInputElement>) => {
             onChange(e.target.checked)
@@ -15,15 +16,16 @@ export default function Switch({ onChange, defaultChecked, checked, ...rest }: S
     )
     return (
         <StyledSwitch checked={checked}>
-            <div className='area' />
+            <div className="area" />
             <input
-                type='checkbox'
+                type="checkbox"
                 onChange={onSwitchChange}
                 defaultChecked={defaultChecked}
                 checked={checked}
+                disabled={disabled}
                 {...rest}
             />
-            <div className='toggle' />
+            <div className="toggle" />
         </StyledSwitch>
     )
 }
