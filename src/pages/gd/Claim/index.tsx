19a32ad74d5a719f0fe0027dc446c22626406e45
Roman Hladilka <felixhack02@gmail.com>
2022-11-25 12:43:31 +0200

    adjust styles

--- src/pages/gd/Claim/index.tsx
+++ src/pages/gd/Claim/index.tsx
@@ -2,29 +2,7 @@ import React, { memo } from 'react'
 import { t } from '@lingui/macro'
 import { useLingui } from '@lingui/react'
 import { ClaimButton, Layout, Title } from '@gooddollar/good-design'
-import { StyleSheet } from 'react-native'
-import { View, Text } from 'native-base'
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
-
-const styles = StyleSheet.create({
-    content: {
-        width: '100%',
-        height: '100%',
-        alignItems: 'center',
-    },
-    notice: {
-        width: '100%',
-        textAlign: 'center',
-        borderRadius: 5,
-        font: {},
-        fontStyle: 'normal',
-        fontWeight: 'bold',
-        fontSize: 14,
-        letterSpacing: 0.35,
-        backgroundColor: '#0d263d66',
-        padding: '40px',
-    },
-})
 
 const Claim = () => {
     const { i18n } = useLingui()
@@ -33,15 +11,17 @@ const Claim = () => {
     return (
         <Layout>
             <Title>{i18n._(t`Claim`)}</Title>
-            <View style={styles.content}>
+            <div className="flex items-center">
                 {account ? (
-                    <ClaimButton firstName={'Test'} method={'redirect'} />
+                    <ClaimButton firstName="Test" method="redirect" />
                 ) : (
-                    <Text color={'white'} style={styles.notice} mt={'5'}>
-                        {i18n._(t`CONNECT A WALLET TO CLAIM YOUR GOODDOLLARS`)}
-                    </Text>
+                    <div className="blue p-40 mt-5 w-full">
+                        <span className="white text-center">
+                            {i18n._(t`CONNECT A WALLET TO CLAIM YOUR GOODDOLLARS`)}
+                        </span>
+                    </div>
                 )}
-            </View>
+            </div>
         </Layout>
     )
 }
