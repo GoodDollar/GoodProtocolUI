cb036b4aaeb367371c25bf917d6dbf753449e3d8
Lewis B <lewis@ikigaistudios.eu>
2022-12-02 08:56:35 +0100

    add theming option to ubeswap component

--- src/pages/gd/Swap/SwapCelo/index.tsx
+++ src/pages/gd/Swap/SwapCelo/index.tsx
@@ -1,22 +1,30 @@
 import React from 'react'
+import { useApplicationTheme } from 'state/application/hooks'
 import Swap from 'ubeswap-swap-dev'
 
 export const UbeSwap = (): JSX.Element => {
+    const [theme, setTheme] = useApplicationTheme()
     return (
         <div
             className="flex flex-col items-center justify-center w-5/6 h-5/6 rounded-3xl"
             style={{
-                backgroundColor: 'rgba(229, 247, 255, 0.52)',
                 padding: '50px 5px',
             }}
         >
             <Swap
+                theme={{
+                    fontFamily: 'Roboto',
+                    primaryColor: '#00b0ff',
+                    userDarkMode: theme === 'dark',
+                }}
                 defaultSwapToken={{
                     address: '0x33265D74abd5ae87cA02A4Fb0C30B7405C8b0682',
                     name: 'GoodDollar',
                     symbol: 'G$',
                     chainId: 42220,
                     decimals: 18,
+                    logoURI:
+                        'https://raw.githubusercontent.com/GoodDollar/GOodProtocolUI/master/src/assets/images/tokens/gd-logo.png',
                 }}
             />
             <span className="flex flex-row items-center justify-center w-full h-2 mt-1 italic font-bold text-gray-400">
