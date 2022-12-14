33da8b2bc282cff67cf2efb1201294f700e2df0e
L03TJ3 <laurence@gooddollar.org>
2022-12-14 13:42:05 +0100

    chore: update lint-staged, partial eslint fix

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,13 +4,15 @@ const { promisify } = require('util')
 
 const eslint = new ESLint()
 const writeFileAsync = promisify(writeFile)
-const tsConfigPath = "./tsconfig.lint-staged.json"
+const tsConfigPath = './tsconfig.lint-staged.json'
 
-const tsConfig = files => `
+const tsConfig = (files) => `
 {
   "extends": "./tsconfig.json",
   "include": [
-    ${files.join(",\n")}
+    "./src/**/*.d.ts",
+    "./types",
+    ${files.join(',\n')}
   ],
   "compilerOptions": {
     "noEmit": true
@@ -19,15 +21,12 @@ const tsConfig = files => `
 `
 
 module.exports = {
-  "src/**/*.{js,jsx,ts,tsx}": async files => {
-    const ignored = await Promise.all(files.map(async file => eslint.isPathIgnored(file)))
-    const filtered = files.filter((_, index) => !ignored[index]).map(path => `"${path}"`);
+    '**/*.{js,jsx,ts,tsx}': async (files) => {
+        const ignored = await Promise.all(files.map(async (file) => eslint.isPathIgnored(file)))
+        const filtered = files.filter((_, index) => !ignored[index]).map((path) => `"${path}"`)
 
-    await writeFileAsync(tsConfigPath, tsConfig(filtered))
+        await writeFileAsync(tsConfigPath, tsConfig(filtered))
 
-    return [
-      `eslint --no-ignore --max-warnings=0 --fix ${filtered.join(' ')}`,
-      `tsc --p ${tsConfigPath}`
-    ]
-  },
+        return [`eslint --no-ignore --max-warnings=0 --fix ${filtered.join(' ')}`, `tsc --p ${tsConfigPath}`]
+    },
 }
--- src/hookstest.ts
+++
@@ -1,4 +0,0 @@
-// TODO: remove this
-export function add(a: number, b: number): number {
-  return a + b;
-}
