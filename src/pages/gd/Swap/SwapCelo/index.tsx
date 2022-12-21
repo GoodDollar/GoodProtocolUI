3985dedf5d467b9517a0c49e36dc97a493153ba7
L03TJ3 <laurence@gooddollar.org>
2022-12-21 09:16:42 +0100

    add: accountinfo and custom connect to swap-component

--- src/pages/gd/Swap/SwapCelo/index.tsx
+++ src/pages/gd/Swap/SwapCelo/index.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { Web3Provider } from '@ethersproject/providers'
+import useActiveWeb3React from 'hooks/useActiveWeb3React'
+import React, { useEffect, useState } from 'react'
 import { useApplicationTheme } from 'state/application/hooks'
 import Swap from 'ubeswap-swap-dev'
+import { useConnectWallet } from '@web3-onboard/react'
+
+interface AccountInfo {
+    account: string
+    explorerUrl: string
+    chainId: number
+    provider: Web3Provider
+}
 
 export const UbeSwap = (): JSX.Element => {
     const [theme] = useApplicationTheme()
+    const { account, library } = useActiveWeb3React()
+    const [accountInfo, setAccountInfo] = useState<AccountInfo | undefined>(undefined)
+    const [, connect] = useConnectWallet()
+
+    useEffect(() => {
+        if (account && library) {
+            setAccountInfo({
+                account: account,
+                explorerUrl: process.env.REACT_APP_CELO_RPC ?? 'https://forno.celo.org',
+                chainId: 42220,
+                provider: library,
+            })
+        }
+    }, [account, library])
 
     return (
         <div
@@ -13,6 +37,8 @@ export const UbeSwap = (): JSX.Element => {
             }}
         >
             <Swap
+                onConnectWallet={connect}
+                accountInfo={accountInfo}
                 theme={{
                     fontFamily: 'Roboto',
                     primaryColor: '#00b0ff',
