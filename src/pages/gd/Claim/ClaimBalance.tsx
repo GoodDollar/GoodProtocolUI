912982f9d3e09ba36d4fe4d6428d32704dee1b5c
L03TJ3 <lewis@ikigaistudios.eu>
2023-02-20 23:46:43 +0100

    draft fix

--- src/pages/gd/Claim/ClaimBalance.tsx
+++ src/pages/gd/Claim/ClaimBalance.tsx
@@ -1,10 +1,10 @@
 import React, { useMemo, useCallback } from 'react'
 import { View, Box, Text } from 'native-base'
-import { ArrowButton, BalanceGD } from '@gooddollar/good-design'
+import { ArrowButton } from '@gooddollar/good-design'
 import { SupportedChains } from '@gooddollar/web3sdk-v2'
 import { useClaim } from '@gooddollar/web3sdk-v2'
-import usePromise from 'hooks/usePromise'
-import { g$Price } from '@gooddollar/web3sdk'
+// import usePromise from 'hooks/usePromise'
+// import { g$Price } from '@gooddollar/web3sdk'
 import { format } from 'date-fns'
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
 import { useClaiming } from 'hooks/useClaiming'
@@ -33,13 +33,13 @@ const ClaimTimer = () => {
 export const ClaimBalance = () => {
     const { claimTime } = useClaim('everyBlock')
     const { chainId } = useActiveWeb3React()
-    const [G$Price] = usePromise(
-        () =>
-            g$Price()
-                .then(({ DAI }) => DAI)
-                .catch(() => undefined),
-        [chainId]
-    )
+    // const [G$Price] = usePromise(
+    //     () =>
+    //         g$Price()
+    //             .then(({ DAI }) => DAI)
+    //             .catch(() => undefined),
+    //     [chainId]
+    // )
 
     const formattedTime = useMemo(() => claimTime && format(claimTime, 'hh aaa'), [claimTime])
     const { switchNetwork } = useEthers()
@@ -60,7 +60,9 @@ export const ClaimBalance = () => {
             <ClaimTimer />
             <Box borderWidth="1" borderColor="borderGrey" width="90%" alignSelf="center" my="2" />
             <Box>
-                <BalanceGD gdPrice={G$Price} />
+                {/* todo-fix: https://github.com/GoodDollar/GoodProtocolUI/issues/337
+                  <BalanceGD gdPrice={G$Price} /> 
+                */}
             </Box>
             <Box alignItems="center">
                 <ArrowButton
