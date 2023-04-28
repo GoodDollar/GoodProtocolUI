4ad08deb2fc08a6e2db0079e252e7d36d35fce1a
Cyber Kevin <emailkevinlarson@gmail.com>
2023-04-28 09:35:45 -0700

    updates for manual merge from saes-dev branch

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -17,6 +17,7 @@ import TransactionUpdater from '../state/transactions/updater'
 import useSendAnalyticsData from 'hooks/useSendAnalyticsData'
 import { isMobile } from 'react-device-detect'
 import { WalletChatWidget } from 'react-wallet-chat'
+import { useConnectWallet, useSetChain } from '@web3-onboard/react'
 
 export const Beta = styled.div`
     font-style: normal;
@@ -62,6 +63,11 @@ function App(): JSX.Element {
     const dispatch = useDispatch<AppDispatch>()
     const [preservedSource, setPreservedSource] = useState('')
     const sendData = useSendAnalyticsData()
+    const [{ chains, connectedChain }] = useSetChain()
+    const [{ wallet }] = useConnectWallet()
+    const connectedAccount = wallet?.accounts[0]
+    const activeChain = chains.find((chain) => chain.id === connectedChain?.id)
+    const chainId = activeChain?.token === 'CELO' ? 42_220 : 1
 
     void useFaucet()
 
@@ -138,8 +144,18 @@ function App(): JSX.Element {
                         <Beta className="mt-3 lg:mt-8">{i18n._(t`This project is in beta. Use at your own risk`)}</Beta>
                     </MainBody>
                 </Wrapper>
+                <WalletChatWidget
+                    style={{ marginBottom: isMobile ? '75px' : '0px' }}
+                    connectedWallet={
+                        connectedAccount &&
+                        chainId && {
+                            walletName: wallet.label,
+                            account: connectedAccount.address,
+                            chainId,
+                        }
+                    }
+                />
             </div>
-            <WalletChatWidget />
         </Suspense>
     )
 }
