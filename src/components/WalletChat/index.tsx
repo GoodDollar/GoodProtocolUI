07264842a45c63afb85bc636b5e5c16d37cf05d4
Cyber Kevin <emailkevinlarson@gmail.com>
2023-05-27 17:10:41 -0700

    using parent provider

--- src/components/WalletChat/index.tsx
+++ src/components/WalletChat/index.tsx
@@ -2,23 +2,11 @@ import React from 'react'
 
 import { WalletChatWidget } from 'react-wallet-chat'
 import { isMobile } from 'react-device-detect'
-import { useActiveWeb3React } from '../../hooks/useActiveWeb3React'
 
 const WalletChat = () => {
-    const { account, chainId, label } = useActiveWeb3React()
-
     return (
         <WalletChatWidget
             style={{ marginBottom: isMobile ? '75px' : '0px' }}
-            connectedWallet={
-                account && chainId
-                    ? {
-                          walletName: label || '',
-                          account: account,
-                          chainId,
-                      }
-                    : undefined
-            }
         />
     )
 }
