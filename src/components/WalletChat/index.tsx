814b8c3cfbfd2d484b7ca912b0aebacdfe663dda
Cyber Kevin <emailkevinlarson@gmail.com>
2023-05-27 17:12:47 -0700

    prettier fix

--- src/components/WalletChat/index.tsx
+++ src/components/WalletChat/index.tsx
@@ -4,11 +4,7 @@ import { WalletChatWidget } from 'react-wallet-chat'
 import { isMobile } from 'react-device-detect'
 
 const WalletChat = () => {
-    return (
-        <WalletChatWidget
-            style={{ marginBottom: isMobile ? '75px' : '0px' }}
-        />
-    )
+    return <WalletChatWidget style={{ marginBottom: isMobile ? '75px' : '0px' }} />
 }
 
 export default WalletChat
