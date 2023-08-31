e1d84adc29c768791a08b3ee6767168ce47b9509
LewisB <lewis@gooddollar.org>
2023-08-31 19:38:55 +0200

    wip: update goodIdmodal, add navigateBack onClose

--- src/pages/gd/GoodId/index.tsx
+++ src/pages/gd/GoodId/index.tsx
@@ -1,11 +1,17 @@
-import React, { memo } from 'react'
+import React, { memo, useCallback } from 'react'
 import { GoodIdModal } from '@gooddollar/good-design'
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
+import { useHistory } from 'react-router-dom'
 
 const GoodId = memo(() => {
     const { account } = useActiveWeb3React()
+    const history = useHistory()
 
-    return <GoodIdModal account={account ?? ''} />
+    const navigateToClaim = useCallback(() => {
+        history.push('/claim')
+    }, [history])
+
+    return <GoodIdModal onClose={navigateToClaim} account={account ?? ''} />
 })
 
 export default GoodId
