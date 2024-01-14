86eaa529aa12c0c284bee45fe74de5869eb8f4cf
LewisB <lewis@gooddollar.org>
2024-01-15 04:26:45 +0800

    fix: wrong minipay fix for wb ui, chore: lingui

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -317,12 +317,12 @@ function AppBar({ sideBar, walletBalance }): JSX.Element {
                                         initial={{
                                             scaleY: 0,
                                             translateY: 20,
-                                            translateX: isSimpleApp ? 17 : isMobile ? 47 : 0,
+                                            translateX: isMinipay ? 17 : isMobile ? 47 : 0,
                                         }}
                                         animate={{
                                             scaleY: 1,
                                             translateY: 0,
-                                            translateX: isMobile ? 47 : 0,
+                                            translateX: isMinipay ? 17 : isMobile ? 47 : 0,
                                             transition: { duration: 250 },
                                         }}
                                     >
