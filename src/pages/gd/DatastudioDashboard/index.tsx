57767b7969a1eb6eee0d2c526abec91ecdc91e5c
LewisB <lewis@gooddollar.org>
2024-02-07 13:51:45 +0800

    fix: dashboard page

--- src/pages/gd/DatastudioDashboard/index.tsx
+++ src/pages/gd/DatastudioDashboard/index.tsx
@@ -1,32 +1,19 @@
 import React from 'react'
-import styled from 'styled-components'
+import { CentreBox } from '@gooddollar/good-design'
 
 const DASHBOARD_URL = process.env.REACT_APP_DATASTUDIO_DASHBOARD_URL || 'https://dashboard.gooddollar.org/'
 
-const Wrapper = styled.div`
-    width: 100%;
-    margin-top: -2.5rem;
-
-    @media only screen and (max-width: 768px) {
-        margin-top: -2rem;
-    }
-
-    @media only screen and (max-width: 480px) {
-        margin-top: -1rem;
-    }
-`
-
 export default function DatastudioDashboard(): JSX.Element {
     return (
-        <Wrapper>
+        <CentreBox h={800} width="100%">
             <iframe
                 title="Datastudio Dashboard"
                 width="100%"
-                height="924"
+                height="900"
                 src={DASHBOARD_URL}
                 style={{ border: 'none' }}
                 allowFullScreen
             ></iframe>
-        </Wrapper>
+        </CentreBox>
     )
 }
