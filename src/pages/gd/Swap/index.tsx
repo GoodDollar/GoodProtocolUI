62e7e059e93d9285e9f516dd4d5d73826ec3d552
LewisB <lewis@gooddollar.org>
2024-03-05 17:03:50 +0800

    fix: use different hook to get network

--- src/pages/gd/Swap/index.tsx
+++ src/pages/gd/Swap/index.tsx
@@ -1,19 +1,18 @@
 import React, { memo } from 'react'
-import { SupportedChains } from '@gooddollar/web3sdk-v2'
+import { SupportedChains, useGetEnvChainId } from '@gooddollar/web3sdk-v2'
 import { usePostHog } from 'posthog-react-native'
 
 import { UniSwap } from './SwapCelo/UniSwap'
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
 import SwapCore from './SwapCore'
-import { getNetworkEnv } from 'utils/env'
 
 const Swap = memo(() => {
     const { chainId } = useActiveWeb3React()
+    const { connectedEnv } = useGetEnvChainId(chainId)
+    const isProd = connectedEnv.includes('production')
     const postHog = usePostHog()
     const payload = postHog?.getFeatureFlagPayload('swap-feature')
     const { fuseEnabled, celoEnabled, reserveEnabled } = (payload as any) || {}
-    const network = getNetworkEnv()
-    const isProd = network.includes('production')
 
     const swapComponentMapping = {
         [SupportedChains.FUSE]: { component: <SwapCore />, enabled: !isProd || (isProd && fuseEnabled) },
