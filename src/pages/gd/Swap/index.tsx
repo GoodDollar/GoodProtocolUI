e8a67b45d5a3b01ab0e37eb9e7c4bd8fb84de629
LewisB <lewis@gooddollar.org>
2024-03-05 15:09:27 +0800

    wip: enable swap on dev

--- src/pages/gd/Swap/index.tsx
+++ src/pages/gd/Swap/index.tsx
@@ -5,17 +5,20 @@ import { usePostHog } from 'posthog-react-native'
 import { UniSwap } from './SwapCelo/UniSwap'
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
 import SwapCore from './SwapCore'
+import { getNetworkEnv } from 'utils/env'
 
 const Swap = memo(() => {
     const { chainId } = useActiveWeb3React()
     const postHog = usePostHog()
     const payload = postHog?.getFeatureFlagPayload('swap-feature')
     const { fuseEnabled, celoEnabled, reserveEnabled } = (payload as any) || {}
+    const network = getNetworkEnv()
+    const isProd = network.includes('production')
 
     const swapComponentMapping = {
-        [SupportedChains.FUSE]: { component: <SwapCore />, enabled: fuseEnabled },
-        [SupportedChains.MAINNET]: { component: <SwapCore />, enabled: reserveEnabled },
-        [SupportedChains.CELO]: { component: <UniSwap />, enabled: celoEnabled },
+        [SupportedChains.FUSE]: { component: <SwapCore />, enabled: !isProd || (isProd && fuseEnabled) },
+        [SupportedChains.MAINNET]: { component: <SwapCore />, enabled: !isProd || (isProd && reserveEnabled) },
+        [SupportedChains.CELO]: { component: <UniSwap />, enabled: !isProd || (isProd && celoEnabled) },
     }
 
     const chainConfig = swapComponentMapping[chainId as any]
