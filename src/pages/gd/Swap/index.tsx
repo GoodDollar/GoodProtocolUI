f1dafe3423f10ad0f5fb8836d7833f447f577e20
sirpy <hadar@gooddollar.org>
2024-03-26 16:49:08 +0200

    fix: posthog featureflags

--- src/pages/gd/Swap/index.tsx
+++ src/pages/gd/Swap/index.tsx
@@ -1,6 +1,6 @@
 import React, { memo } from 'react'
 import { SupportedChains } from '@gooddollar/web3sdk-v2'
-import { usePostHog } from 'posthog-react-native'
+import { useFeatureFlagWithPayload } from 'posthog-react-native'
 
 import { UniSwap } from './SwapCelo/UniSwap'
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
@@ -8,8 +8,8 @@ import SwapCore from './SwapCore'
 
 const Swap = memo(() => {
     const { chainId } = useActiveWeb3React()
-    const postHog = usePostHog()
-    const payload = postHog?.getFeatureFlagPayload('swap-feature')
+    const payload = useFeatureFlagWithPayload('swap-feature')
+
     const { fuseEnabled, celoEnabled, reserveEnabled } = (payload as any) || {}
 
     const swapComponentMapping = {
