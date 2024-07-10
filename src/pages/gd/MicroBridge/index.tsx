e5a17f1eb33ceab085413e52a1d12cd58376731b
LewisB <lewis@gooddollar.org>
2024-07-11 03:48:49 +0900

    add: microbridge with updated design

--- src/pages/gd/MicroBridge/index.tsx
+++ src/pages/gd/MicroBridge/index.tsx
@@ -6,23 +6,39 @@ import {
     MicroBridgeController,
     // IClaimCard,
 } from '@gooddollar/good-design'
+import { Text, VStack } from 'native-base'
+import { i18n } from '@lingui/core'
+import { t } from '@lingui/macro'
+
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
+import { PageLayout } from 'components/Layout/PageLayout'
 
 const MicroBridge = memo(() => {
     const { account } = useActiveWeb3React()
 
     return (
-        <div className="rounded p-14">
-            {account && (
-                <>
-                    <SwitchChainModal>
-                        <WalletAndChainGuard validChains={[122, 42220]}>
-                            <MicroBridgeController withRelay={false} />
-                        </WalletAndChainGuard>
-                    </SwitchChainModal>
-                </>
-            )}
-        </div>
+        <PageLayout title="Micro Bridge" faqType="bridge">
+            <VStack space={2} textAlign="center" justifyContent="center" alignItems="center" pb={2}>
+                <Text fontFamily="subheading" fontSize="sm" color="goodGrey.600" pt={4} pb={8} textAlign="center">
+                    {i18n._(
+                        t`Seamlessly convert between Fuse G$ tokens to Celo and vice versa, 
+enabling versatile use of G$ tokens across various platforms and 
+ecosystems`
+                    )}
+                </Text>
+            </VStack>
+            <div className="rounded px-14 py-4">
+                {account && (
+                    <>
+                        <SwitchChainModal>
+                            <WalletAndChainGuard validChains={[122, 42220]}>
+                                <MicroBridgeController withRelay={false} />
+                            </WalletAndChainGuard>
+                        </SwitchChainModal>
+                    </>
+                )}
+            </div>
+        </PageLayout>
     )
 })
 
