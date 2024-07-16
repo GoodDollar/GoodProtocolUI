e7d5e493359c367287de3a4d0e97d4d81b0d915c
LewisB <lewis@gooddollar.org>
2024-07-17 01:03:22 +0900

    fix: bridge styling

--- src/pages/gd/MicroBridge/index.tsx
+++ src/pages/gd/MicroBridge/index.tsx
@@ -10,12 +10,9 @@ import { Text, VStack } from 'native-base'
 import { i18n } from '@lingui/core'
 import { t } from '@lingui/macro'
 
-import useActiveWeb3React from 'hooks/useActiveWeb3React'
 import { PageLayout } from 'components/Layout/PageLayout'
 
 const MicroBridge = memo(() => {
-    const { account } = useActiveWeb3React()
-
     return (
         <PageLayout title="Micro Bridge" faqType="bridge">
             <VStack space={2} textAlign="center" justifyContent="center" alignItems="center" pb={2}>
@@ -27,16 +24,14 @@ ecosystems`
                     )}
                 </Text>
             </VStack>
-            <div className="rounded px-14 py-4">
-                {account && (
-                    <>
-                        <SwitchChainModal>
-                            <WalletAndChainGuard validChains={[122, 42220]}>
-                                <MicroBridgeController withRelay={false} />
-                            </WalletAndChainGuard>
-                        </SwitchChainModal>
-                    </>
-                )}
+            <div className="rounded max-w-3xl min-w-96 w-full mx-auto">
+                <VStack maxWidth="800" minWidth="384" width="100%" alignItems="center" justifyContent="center">
+                    <SwitchChainModal>
+                        <WalletAndChainGuard validChains={[122, 42220]}>
+                            <MicroBridgeController withRelay={false} />
+                        </WalletAndChainGuard>
+                    </SwitchChainModal>
+                </VStack>
             </div>
         </PageLayout>
     )
