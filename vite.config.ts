6f60d8cdf1aab723eefaab3b6b5e7b1648cfb2c9
LewisB <lewis@gooddollar.org>
2024-11-30 08:37:22 +0100

    add: gd sdk's with goodid flow + vite fixes

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { lingui } from '@lingui/vite-plugin'
 import dynamicImports from 'vite-plugin-dynamic-import'
 import { visualizer } from 'rollup-plugin-visualizer'
 import dotenv from 'dotenv'
+import * as esbuild from 'esbuild'
 
 dotenv.config()
 
@@ -25,6 +26,17 @@ if (process.env.HTTPS === 'true') {
 } else {
     https = false
 }
+
+const jsxTransform = (matchers: RegExp[]) => ({
+    name: 'js-in-jsx',
+    load(id: string) {
+        if (matchers.some((matcher) => matcher.test(id)) && id.endsWith('.js')) {
+            const file = fs.readFileSync(id, { encoding: 'utf-8' })
+            return esbuild.transformSync(file, { loader: 'jsx', jsx: 'transform' })
+        }
+    },
+})
+
 export default defineConfig(({ command, mode }) => {
     process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
     return {
@@ -69,13 +81,19 @@ export default defineConfig(({ command, mode }) => {
                 'react-native-svg': 'react-native-svg-web',
                 'react-native-webview': 'react-native-web-webview',
                 'lottie-react-native': 'react-native-web-lottie',
+                '@react-native-clipboard/clipboard': 'react-native-web-clipboard',
                 jsbi: path.resolve(__dirname, '.', 'node_modules', 'jsbi', 'dist', 'jsbi-cjs.js'), // https://github.com/Uniswap/sdk-core/issues/20#issuecomment-1559863408
             },
             dedupe: ['react', 'ethers', 'react-dom', 'native-base'],
         },
         build: {
             commonjsOptions: {
+                extensions: ['.js', '.jsx', '.web.js', '.web.jsx'],
                 transformMixedEsModules: true, //handle deps that use "require" and "module.exports"
+                include: [/node_modules/],
+            },
+            rollupOptions: {
+                plugins: [jsxTransform([/react-native-.*\.jsx?$/])], //for some reason react-native packages are not being transpiled even with esbuild jsx settings
             },
         },
         define: {
