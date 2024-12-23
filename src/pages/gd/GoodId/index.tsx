8b0fbd9845458a3c0d3ea7306b22002e04da4bdd
sirpy <hadar@gooddollar.org>
2024-12-23 15:43:49 +0200

    add: goodid by country feature flag

--- src/pages/gd/GoodId/index.tsx
+++ src/pages/gd/GoodId/index.tsx
@@ -10,13 +10,29 @@ import GoodIdDetails from './GoodIdDetails'
 import { Onboard } from './Onboard'
 import { PageLayout } from 'components/Layout/PageLayout'
 import usePromise from 'hooks/usePromise'
+import { retry } from 'utils/retry'
+
+export const isSupportedCountry = async (supportedCountries = '') => {
+    if (!supportedCountries) return false
+
+    const country = await retry(() => fetch('https://get.geojs.io/v1/ip/country.json'), {
+        n: 3,
+        minWait: 500,
+        maxWait: 1500,
+    })
+        .promise.then((_) => _.json())
+        .then((data) => data.country)
+        .catch(() => false)
+
+    return supportedCountries?.split(',')?.includes(country)
+}
 
 const GoodId = () => {
     const { account = '' } = useEthers()
     const { certificateSubjects, isWhitelisted } = useGoodId(account)
     const [skipSegmentation, setSkipSegmentation] = useState<boolean | undefined>(false)
     const [, payload] = useFeatureFlagWithPayload('goodid')
-    const { enabled = false, whitelist } = payload ?? {}
+    const { enabled = false, whitelist = [], countries = '' } = payload ?? {}
 
     const [isUpgraded] = usePromise(async () => {
         if (isEmpty(certificateSubjects) && isWhitelisted !== undefined) {
@@ -27,27 +43,25 @@ const GoodId = () => {
         return await AsyncStorage.getItem('goodid_upgraded')
     }, [certificateSubjects, isWhitelisted])
 
+    const [isGoodIdEnabled] = usePromise(async () => {
+        if (enabled || whitelist?.includes(account)) return true
+
+        return isSupportedCountry(countries)
+    }, [enabled, whitelist, countries, account])
+
     const onExit = useCallback(() => {
         setSkipSegmentation(isUpgraded === true)
     }, [isUpgraded])
 
-    if (isWhitelisted === undefined || isUpgraded === undefined) return <Spinner variant="page-loader" size="lg" />
-
-    // Only for UAT and segmented release.
-    if (!enabled && !whitelist?.includes(account)) {
-        return (
-            <PageLayout faqType="goodid">
-                <VStack margin="auto" mt="0">
-                    <GoodIdDetails />
-                </VStack>
-            </PageLayout>
-        )
-    }
+    if (isGoodIdEnabled === undefined || isWhitelisted === undefined || isUpgraded === undefined)
+        return <Spinner variant="page-loader" size="lg" />
 
     return (
         <PageLayout faqType="goodid">
             <VStack margin="auto" mt="0" maxW="650">
-                {(isWhitelisted && !isEmpty(certificateSubjects) && isUpgraded) || skipSegmentation ? (
+                {(isWhitelisted && !isEmpty(certificateSubjects) && isUpgraded) ||
+                skipSegmentation ||
+                !isGoodIdEnabled ? (
                     <GoodIdDetails />
                 ) : (
                     <Onboard onExit={onExit} />
