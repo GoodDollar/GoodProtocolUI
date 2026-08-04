104f36ac5733c05e918575e20177bf3c20b66372
LewisB <laurence@gooddollar.org>
2026-08-04 14:34:22 +0700

    qa-push: push latest buygd changes, for testing in preview environment

--- src/pages/gd/BuyGD/index.tsx
+++ src/pages/gd/BuyGD/index.tsx
@@ -90,6 +90,7 @@ const BuyGd = memo(() => {
                 <GdOnramperWidget
                     isTesting={!isProd}
                     onEvents={handleEvents}
+                    env="production"
                     apiKey={process.env.REACT_APP_ONRAMPER_KEY}
                 />
             </Box>
